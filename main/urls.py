6dd252058c5a5b132b159020cb67c2b610d25968
Om Suman <om.suman514@gmail.com>
2025-06-15 18:18:51 +0530

    commit changes

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from .views import (
-     home, login_view, signup_view, logout_view,dashboard,profile, change_password,vote,verify_otp_view, resend_otp_view
+     home, login_view, signup_view, logout_view,dashboard,profile, change_password,vote,verify_otp_view
     )
 from django.conf import settings
 from django.conf.urls.static import static
@@ -13,7 +13,7 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('signup/', signup_view, name='signup'),
     path("verify-otp/", verify_otp_view, name="verify_otp"),
-    path("resend-otp/", resend_otp_view, name="resend_otp"),
+    #path("resend-otp/", resend_otp_view, name="resend_otp"),
     path('logout/', logout_view, name='logout'),
     path('vote/', vote, name='vote'),
 ]
